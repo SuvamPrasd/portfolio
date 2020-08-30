c05eb79b0afe90b0459412c3fa90186e74b4ddab
SuvamPrasd <tituprasad30@gmail.com>
2020-08-30 19:50:08 +0530

    fixed-design

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ anime({
     scale: 1.3,
     duration: 1000,
 });
+
+var card = document.querySelector(".card");
+var playing = false;
+
+card.addEventListener('click', function () {
+    if (playing)
+        return;
+
+    playing = true;
+    anime({
+        targets: card,
+        scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+        rotateY: { value: '+=180', delay: 200 },
+        easing: 'easeInOutSine',
+        duration: 400,
+        complete: function (anim) {
+            playing = false;
+        }
+    });
+});
