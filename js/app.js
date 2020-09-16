856d22e00eddbd52bff957ea522d9ce042b7222a
suvam prasad <tituprasad30@gmail.com>
2020-09-16 12:09:15 +0530

    updatedv1.9

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAE0LSd2woLl3nSoxucHuVbpFSmMHp35Qc",
+    authDomain: "portfolio-3f38e.firebaseapp.com",
+    databaseURL: "https://portfolio-3f38e.firebaseio.com",
+    projectId: "portfolio-3f38e",
+    storageBucket: "portfolio-3f38e.appspot.com",
+    messagingSenderId: "1087898606033",
+    appId: "1:1087898606033:web:2bdd1742f112f692b528f0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
